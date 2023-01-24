1e1e133674ee742caae47230f2458dc73fe294ea
Darian Fultz <dfultz826@yahoo.com>
2023-01-24 17:49:47 -0500

    Periodic save

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
-      <h1 style={{display: 'flex', justifyContent: 'center'}}>AUTHOR BUCKET LIST</h1>
+    <div className='center'>
+      <h1>AUTHOR BUCKET LIST</h1>
       <br></br>
       <p>Welcome to my <strong>Author Bucket List application</strong>.  Here, I visually display authors and their books that I've read.  I can attach a personal note about a particular selection.  In addition, someone else can leave a comment or review about a particular book.  Feel free to peruse my list.  It's in the early stages.  I hope to grow it every month!</p>
       <br></br>
-      <video controls style={{display: 'flex', justifyContent: 'center'}}>
+      <video controls>
         <source src="https://media.gettyimages.com/id/1003426724/nl/video/sequence-showing-stephen-king-typing-on-his-laptop-at-his-home-in-bangor-maine.mp4?s=mp4-640x640-gi&k=20&c=yFsx586av4RxfcAIoK4IUO6MBv8_OSv9UsXu7r6qoBU=" alt="Stephen King typing"/>
       </video>
       <br></br>
